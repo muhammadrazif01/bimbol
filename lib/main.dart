a573f5b12d3e922e371b85f4d8424d0fc7270fbc
Muhammad Razif Firmansyah <muhammad.razif01@ui.ac.id>
2021-12-09 10:15:18 +0700

    Basic homepage

--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,207 @@
+// some part of this code is adapted and modified from flutter api
+// (https://api.flutter.dev/flutter/material/BottomNavigationBar-class.html)
+
+// Import material package (very important!)
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+// Run MyApp as main function
+void main() => runApp(const MyApp());
 
+// MyApp as main widget container
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      // title : set menjadi bimbol
+      title: 'BIMBOL',
+
+      // theme : atur tema menjadi warna ungu dan font menjadi Raleway
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.purple,
+        fontFamily: 'Raleway',
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+
+      // Set home untuk constructor MyStatefulWidget()
+      home: MyStatefulWidget(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
+// StatefulWidget yang akan diinitiate
+class MyStatefulWidget extends StatefulWidget {
+  const MyStatefulWidget({Key? key}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+  // Set status untuk StatefulWidget
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<MyStatefulWidget> createState() => _MyStatefulWidgetState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+// Buat kelas private untuk state dari StatefulWidget
+class _MyStatefulWidgetState extends State<MyStatefulWidget> {
+  // Atur setting untuk state dari Stateful Widget
+  int _selectedIndex = 0;
+  static const TextStyle optionStyle = TextStyle(
+      fontFamily: 'Raleway', fontSize: 30, fontWeight: FontWeight.w900);
+  static const List<Widget> _widgetOptions = <Widget>[
+    // Text untuk bagian mainpage
+    Text(
+      'This contains button for information',
+      style: optionStyle,
+    ),
 
-  void _incrementCounter() {
+    // Text untuk bagian daftar
+    Text(
+      'Untuk halaman daftar (not implemented yet here)',
+      style: optionStyle,
+    ),
+
+    // Text untuk bagian masuk
+    Text(
+      'Untuk halaman masuk (not implemented yet here)',
+      style: optionStyle,
+    ),
+  ];
+
+  // Define untuk set state saat navbar diklik
+  void _onItemTapped(int index) {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _selectedIndex = index;
     });
   }
 
+  // Build
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
+      // Appbar
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: const Text(
+          'BIMBOL\nBimbel di Masa Pandemi',
+          textAlign: TextAlign.center,
+          style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+        ),
+        centerTitle: true,
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+
+      // Set bg color
+      backgroundColor: Colors.purple[300],
+
+      // Set body (sebagian dari kode ini terinspirasi oleh https://youtu.be/4wS5LdXJgEA)
+      body: Container(
+          padding: EdgeInsets.all(12),
+          child: Stack(fit: StackFit.expand, children: [
+            buildBackground(),
+            Center(
+              child: buildCenterText(),
             ),
+            Align(
+              alignment: Alignment(0.0, 1),
+              child: buildLikeFB(),
+            ),
+            Positioned(
+              right: 8,
+              top: 4,
+              child: buildLogo(),
+            ),
+          ])),
+
+      // Set untuk navbar (sebagian dari kode ini didapat dari Flutter API)
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Mainpage',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.school),
+            label: 'Daftar',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Masuk',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.purple,
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+
+  // Widget-widget dibawah diadaptasi, dimodifikasi, dan terinspirasi dari https://youtu.be/4wS5LdXJgEA
+
+  // Buat background
+  Widget buildBackground() => ClipRRect(
+        borderRadius: BorderRadius.circular(24),
+        child: Container(
+          //color: Colors.black,
+          child: Opacity(
+            opacity: 0.8,
+            child: Image.asset(
+              'assets/image/adobeproject1.png',
+              fit: BoxFit.fitHeight,
+            ),
+          ),
+        ),
+      );
+
+  // Buat tulisan center
+  Widget buildCenterText() => Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Text(
+            'BIMBOL',
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 32,
+              color: Colors.white,
+            ),
+          ),
+          const SizedBox(height: 16),
+          Container(
+            padding: EdgeInsets.symmetric(horizontal: 42),
+            child: Text(
+              'Sebuah Solusi Mudah dan Aman di Masa Pandemi',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 20,
+                color: Colors.white,
+              ),
+              textAlign: TextAlign.center,
+            ),
+          ),
+        ],
+      );
+
+  // Build like us on FB
+  Widget buildLikeFB() => Container(
+        color: Colors.white,
+        padding: EdgeInsets.symmetric(vertical: 12),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(Icons.favorite, color: Colors.red),
+            const SizedBox(width: 8),
             Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+              'Likes Us on Facebook!',
+              style: TextStyle(
+                color: Colors.red,
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+              ),
             ),
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
+      );
+
+  // Build logo UI
+  Widget buildLogo() => IconButton(
+        icon: ImageIcon(
+          AssetImage('assets/image/logoui.png'),
+          color: Colors.yellow,
+        ),
+        onPressed: () {},
+      );
 }
