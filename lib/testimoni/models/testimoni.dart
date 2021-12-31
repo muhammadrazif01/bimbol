e19245131a44031999070075f1aa072672952d8d
Mutiara Azzahra <89289936+mutiarazzahra@users.noreply.github.com>
2021-12-31 22:59:26 +0700

    update

--- lib/testimoni/models/testimoni.dart
+++ lib/testimoni/models/testimoni.dart
@@ -1,1 +1,45 @@
+import 'dart:convert';
+
+class Testimoni {
+  int id;
+  String nama;
+  String kelas;
+  String testimoni;
+
+  Testimoni({
+    this.id = 0,
+    required this.testimoni,
+    required this.nama,
+    required this.kelas
+  });
+
+  factory Testimoni.fromJson(Map<String, dynamic> json) {
+    return Testimoni(
+        id: json["id"], 
+        nama: json["nama"], 
+        kelas: json["kelas"], 
+        testimoni: json["testimoni"]);
+  }
+
+  Map<String, dynamic> toJson() {
+    return {"id": id, "nama": nama, "kelas": kelas, "testimoni": testimoni};
+  }
+
+  @override
+  String toString() {
+    return 'Testimoni{id: $id, nama: $nama, kelas: $kelas, testimoni: $testimoni}';
+  }
+
+}
+
+String testimoniToJson(Testimoni data) {
+  final jsonData = data.toJson();
+  return json.encode(jsonData);
+}
+
+List<Testimoni> testimoniFromJson(String jsonData) {
+  final data = json.decode(jsonData);
+  return List<Testimoni>.from(data.map((item) => Testimoni.fromJson(item)));
+  
+}
 
